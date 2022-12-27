9da3e58b272f1f70641d0370b66c6b689d08c56f
MahimaSanketh <mahimadevinfo@gmail.com>
2022-12-27 10:47:05 +0530

    rock-paper-scissor game

--- demo.py
+++ demo.py
@@ -2,11 +2,15 @@
 import random
 print('Enter "rock / paper / scissor"')
 score_user = 0
+loops = 0
 socre_bot = 0
-while 0 <= 10:  # this while loops use for itration this code (infinity)
+score_lavel = int(input('Enter Score value :  '))
+# this while loops use for itration this code (infinity)
+while loops <= score_lavel:
     user_value = input('enter your key :  ')
     game_list = ['rock', 'paper', 'scissor']
     bot_value = random.choice(game_list)
+    loops += 1
 
     if bot_value == 'rock' and user_value == 'paper' or bot_value == 'paper' and user_value == 'scissor' or bot_value == 'scissor ' and user_value == 'rock':
         print('you won ')
@@ -22,3 +26,8 @@ while 0 <= 10:  # this while loops use for itration this code (infinity)
         print('bot choice is  ' + bot_value)
         socre_bot += 1
         print('Bot score is : ' + str(socre_bot))
+if score_user > score_lavel:
+    print('You won this series$$')
+
+else:
+    print('you lost this series$$')
